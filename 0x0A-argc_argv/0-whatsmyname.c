075b06772ec19eb139664053c176320d64bcbeba
KaraboNkgotho <karabomakgethe@gmail.com>
2024-09-19 02:58:08 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+
+ * main - a program that prints the name
+
+ * @argc: no of arguments
+
+ * @argv: array
+
+ * Return: 0
+
+ */
+	
+int main(int argc __attribute__((unused)), char *argv[])
+
+{
+
+	printf("%s\n", *argv);
+	return (0);
+		
+}
